c9f1be42bb9e6c42512079c78a6a27881d4943e9
krekz <kfitri559@gmail.com>
2024-01-24 22:31:48 +0800

    fixing header

--- header_footer.js
+++ header_footer.js
@@ -4,7 +4,7 @@ function includeHeaderAndFooter() {
     <header>
     <nav class="navbar navbar-expand-lg  ">
         <div class="container   ">
-          <a class="navbar-brand fs-3 text-dark" href="#">sakamelow.</a>
+          <a class="navbar-brand fs-3 text-dark" href="index.html">sakamelow.</a>
           <button class="navbar-toggler align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
